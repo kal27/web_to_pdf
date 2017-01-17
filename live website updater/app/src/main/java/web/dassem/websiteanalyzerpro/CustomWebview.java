700f8f852b9fe76364238bc9af7c40fef70deaf2
fameempathy <fame_empathy@o2.pl>
2017-01-17 19:22:11 +0100

    second commit

---
+++ live website updater/app/src/main/java/web/dassem/websiteanalyzerpro/CustomWebview.java
@@ -0,0 +1,32 @@
+package web.dassem.websiteanalyzerpro;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class CustomWebview extends WebView {
+
+
+    public CustomWebview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setDesktopMode(final boolean enabled) {
+        final WebSettings webSettings = getSettings();
+
+        final String newUserAgent;
+        if (enabled) {
+            newUserAgent = webSettings.getUserAgentString().replace("Mobile", "eliboM").replace("Android", "diordnA");
+        } else {
+            newUserAgent = webSettings.getUserAgentString().replace("eliboM", "Mobile").replace("diordnA", "Android");
+        }
+
+        webSettings.setUserAgentString(newUserAgent);
+        webSettings.setUseWideViewPort(enabled);
+        webSettings.setLoadWithOverviewMode(enabled);
+        webSettings.setSupportZoom(enabled);
+        webSettings.setBuiltInZoomControls(enabled);
+    }
+}
